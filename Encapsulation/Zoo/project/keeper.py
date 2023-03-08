57b6ccd751ba037f7dbcab2947bd6c2dd8a1f20b
Teodor Teodorov <117835130+TeodorTeodorov@users.noreply.github.com>
2023-03-08 09:23:04 +0200

    Add files via upload

---
+++ Encapsulation/Zoo/project/keeper.py
@@ -0,0 +1,8 @@
+from project.worker import Worker
+
+
+class Keeper(Worker):
+    pass
+
+    # def __init__(self,name, age, salary):
+    #     super().__init__(name, age, salary)
---
+++ Encapsulation/Zoo/project/vet.py
@@ -0,0 +1,8 @@
+from project.worker import Worker
+
+
+class Vet(Worker):
+    pass
+
+    # def __init__(self,name, age, salary):
+    #     super().__init__(name, age, salary)
